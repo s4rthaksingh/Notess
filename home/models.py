d4dacd2b96a68ab58ac958398de626364be175e3
Sarthak <sarthakss898@gmail.com>
2025-05-18 13:02:57 +0530

    Add activies
    Users can now see what others are doing in the recent activity feed

--- home/models.py
+++ home/models.py
@@ -17,3 +17,11 @@ class Note(models.Model):
     description = models.TextField(max_length=5000)
     def __str__(self):
         return self.title
+    
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=200)
+    time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.message} at {self.time.strftime('%I:%M %p')}"
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from .models import Note,Notebook
+from .models import Note,Notebook,Activity
 from .forms import NoteForm, RegisterForm,NotebookForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -12,6 +12,7 @@ def index(request):
     context={
         'user' : request.user,
         'notebooks': Notebook.objects.filter(user=request.user),
+        'activities': Activity.objects.all().order_by('-time')
     }
     return render(request,'viewnotebooks.html',context=context)
 
@@ -21,7 +22,8 @@ def viewnote(request,noteid):
     if request.user != note.user and not note.notebook.is_public:
         return HttpResponse("This note doesn't exist")
     context={
-        'note' : note
+        'note' : note,
+        'activities': Activity.objects.all().order_by('-time')
     }
     return render(request,'viewnote.html',context=context)
 
@@ -32,7 +34,8 @@ def viewnotebook(request,notebookid):
     notebook = Notebook.objects.get(pk=notebookid)
     context={
         'notes' : Note.objects.filter(notebook=notebook),
-        'user' : request.user
+        'user' : request.user,
+        'activities': Activity.objects.all().order_by('-time')
     }
     return render(request,'index.html',context=context)
 
@@ -45,10 +48,12 @@ def create(request):
             note = form.save(commit=False)
             note.user = request.user
             note.save()
-            return HttpResponseRedirect("/")  
+            Activity.objects.create(user=request.user, message="created a new note")
+            return HttpResponseRedirect("/")
     else:
         form = NoteForm(user=request.user)
-    return render(request,'create.html',{'form':form})
+    context = {'form':form, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'create.html',context)
 
 @login_required(login_url="/login")
 def createnotebook(request):
@@ -58,17 +63,22 @@ def createnotebook(request):
             notebook = form.save(commit=False)
             notebook.user = request.user
             notebook.save()
+            Activity.objects.create(user=request.user, message="created a new notebook")
             return HttpResponseRedirect("/")
     else:
         form = NotebookForm(user=request.user)
-    return render(request,'createnotebook.html',{'form':form})
+    context = {'form':form, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'createnotebook.html',context)
 
 @login_required(login_url="/login")
 def delete(request,noteid):
     if request.user != Note.objects.get(pk=noteid).user:
         return HttpResponse("This note doesn't exist")
     else:
-        Note.objects.get(pk=noteid).delete()
+        note = Note.objects.get(pk=noteid)
+        Activity.objects.create(user=request.user, message="deleted a note")
+        note.delete()
+    context = {'activities': Activity.objects.all().order_by('-time')}
     return HttpResponseRedirect('/')
 
 @login_required(login_url="/login")
@@ -76,7 +86,10 @@ def deletenotebook(request,notebookid):
     if request.user != Notebook.objects.get(pk=notebookid).user:
         return HttpResponse("This note doesn't exist")
     else:
-        Notebook.objects.get(pk=notebookid).delete()
+        notebook = Notebook.objects.get(pk=notebookid)
+        Activity.objects.create(user=request.user, message="deleted a notebook")
+        notebook.delete()
+    context = {'activities': Activity.objects.all().order_by('-time')}
     return HttpResponseRedirect('/')
 
 @login_required(login_url="/login")
@@ -86,22 +99,26 @@ def edit(request,noteid):
     if request.method == 'POST':
         form = NoteForm(request.POST,instance=Note.objects.get(pk=noteid),user=request.user)
         if form.is_valid():
-            form.save()
+            note = form.save()
+            Activity.objects.create(user=request.user, message="edited a note")
             return HttpResponseRedirect('/')
     else:
         form = NoteForm(instance=Note.objects.get(pk=noteid),user=request.user)
-    return render(request,'edit.html',{'form':form})
+    context = {'form':form, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'edit.html',context)
 
 @login_required(login_url="/login")
 def editnotebook(request,notebookid):
     if request.method == 'POST':
         form = NotebookForm(request.POST,instance=Notebook.objects.get(pk=notebookid),user=request.user)
         if form.is_valid():
-            form.save()
+            notebook = form.save()
+            Activity.objects.create(user=request.user, message="edited a notebook")
             return HttpResponseRedirect('/')
     else:
         form = NotebookForm(instance=Notebook.objects.get(pk=notebookid),user=request.user)
-    return render(request,'editnotebook.html',{'form':form})
+    context = {'form':form, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'editnotebook.html',context)
 
 @login_required
 def share(request,noteid):
@@ -117,13 +134,17 @@ def share(request,noteid):
         )
         newnote = Note(title=f'{note.title} - Shared by {note.user}',description=note.description,user=user,notebook=notebook)
         newnote.save()
+        user_id = int(request.POST['dropdown'])
+        user = User.objects.all().get(pk=user_id)
         return HttpResponse(f'Successfully shared note "{Note.objects.get(pk=noteid)}" with {user.username}')
     else:
-        return render(request,'share.html',{'users':User.objects.all()})
+        context = {'users':User.objects.all(), 'activities': Activity.objects.all().order_by('-time')}
+        return render(request,'share.html',context)
 
 def loginUser(request):
     if request.user.is_authenticated:
-        return render(request,'already.html',{'title':'Log in to another account','action':'log in to another account'})
+        context = {'title':'Log in to another account','action':'log in to another account', 'activities': Activity.objects.all().order_by('-time')}
+        return render(request,'already.html',context)
     if request.method=='POST':
         username = request.POST.get("username").lower()
         password = request.POST.get("password").lower()
@@ -139,16 +160,19 @@ def loginUser(request):
         else:
             login(request,user)
             return HttpResponseRedirect('/')
-    return render(request,'login.html')
+    context = {'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'login.html', context)
 
 @login_required(login_url="/login")
 def logoutUser(request):
     logout(request)
+    context = {'activities': Activity.objects.all().order_by('-time')}
     return HttpResponseRedirect('/')
 
 def register(request):
     if request.user.is_authenticated:
-        return render(request,'already.html',{'title':'Register a new account','action':'register a new account'})
+        context = {'title':'Register a new account','action':'register a new account', 'activities': Activity.objects.all().order_by('-time')}
+        return render(request,'already.html',context)
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -156,14 +180,17 @@ def register(request):
             user.username = user.username.lower()
             user.save()
             login(request,user)
+            Activity.objects.create(user=user, message="just signed up!")
             return HttpResponseRedirect('/')
     else:
         form = RegisterForm()
-    return render(request,'register.html',{'form':form})
+    context = {'form':form, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'register.html',context)
 
 def users(request):
     users = User.objects.all()
-    return render(request,'users.html',{'users':users})
+    context = {'users':users, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'users.html',context)
 
 def viewuser(request,username):
     print(username)
@@ -172,4 +199,5 @@ def viewuser(request,username):
     public_notebook = Notebook.objects.filter(user=user,is_public=True).first()
     print(public_notebook)
     notes = Note.objects.filter(user=user,notebook=public_notebook)
-    return render(request,'profile.html',{'notebook':public_notebook,'notes':notes,'user':user})
+    context = {'notebook':public_notebook,'notes':notes,'user':user, 'activities': Activity.objects.all().order_by('-time')}
+    return render(request,'profile.html',context)
